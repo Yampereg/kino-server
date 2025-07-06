7e22d63f0cbfd89834d60de9b90e296763319f25
Yampereg <Yampereg123@gmail.com>
2025-07-06 16:36:20 +0300

    Heap overflow fix - loading with pages

--- src/main/java/com/example/kino/film/FilmRepository.java
+++ src/main/java/com/example/kino/film/FilmRepository.java
@@ -1,6 +1,8 @@
 package com.example.kino.film;
 
 import com.example.kino.user.User;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
