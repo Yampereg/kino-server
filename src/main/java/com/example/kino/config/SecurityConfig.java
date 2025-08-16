cad9d13d29370a447ff8825410580c5090f61f5a
Yampereg <Yampereg123@gmail.com>
2025-08-16 12:21:57 +0300

    implemented token expiry handlers

--- src/main/java/com/example/kino/config/SecurityConfig.java
+++ src/main/java/com/example/kino/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
